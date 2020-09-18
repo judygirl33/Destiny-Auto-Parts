c3063a92ba78900497623ddb6db7fc62946bb378
Fábio Emilio Costa <fabiocosta0305@gmail.com>
2020-09-18 15:08:16 -0300

    New edits by Ivanna

--- CODE/PARTEDIT.cbl
+++ CODE/PARTEDIT.cbl
@@ -3,6 +3,9 @@
        AUTHOR. IVANNA COLÁN.
       ******************************************************************
       * validates the data of the PARTs PORTION OF a PARTSUPP record
+      *9/18 used 05 variables instead of PARTS group variable to avoid
+      * issue with COMP field weeks-lead-time. It still shows incorrect
+      *  ly value fo that field.
       ******************************************************************
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
@@ -13,10 +16,32 @@
        01 WS-VEHICLE-YEAR-INT          PIC 9(04).
 
        LINKAGE SECTION.
-       COPY PARTS. *>Parts Copybook
+      *COPY PARTS. *>Parts Copybook
+       01  PART-NUMBER       PIC X(23) VALUE SPACES.
+       01  PART-NAME         PIC X(14) VALUE SPACES.
+       01  SPEC-NUMBER       PIC X(07) VALUE SPACES.
+       01  GOVT-COMML-CODE   PIC X(01) VALUE SPACES.
+       01  BLUEPRINT-NUMBER  PIC X(10) VALUE SPACES.
+       01  UNIT-OF-MEASURE   PIC X(03) VALUE SPACES.
+       01  WEEKS-LEAD-TIME   PIC S9(04) COMP VALUE ZEROS.
+       01  VEHICLE-MAKE      PIC X(03) VALUE SPACES.
+                88 CHRYSLER       VALUE 'CHR'.
+                88 FORD           VALUE 'FOR'.
+                88 GM             VALUE 'GM '.
+                88 VOLKSWAGON     VALUE 'VW '.
+                88 TOYOTA         VALUE 'TOY'.
+                88 JAGUAR         VALUE 'JAG'.
+                88 PEUGEOT        VALUE 'PEU'.
+                88 BMW            VALUE 'BMW'.
+       01  VEHICLE-MODEL     PIC X(10) VALUE SPACES.
+       01  VEHICLE-YEAR      PIC X(04) VALUE '0000'.
+
        01 LS-PARTEDIT-ERRORCOUNTER      PIC 9(02) VALUE ZERO.
 
-       PROCEDURE DIVISION USING PARTS, LS-PARTEDIT-ERRORCOUNTER.
+       PROCEDURE DIVISION USING PART-NUMBER, PART-NAME,
+           SPEC-NUMBER, GOVT-COMML-CODE, BLUEPRINT-NUMBER,
+           UNIT-OF-MEASURE, WEEKS-LEAD-TIME, VEHICLE-MAKE,
+           VEHICLE-MODEL, VEHICLE-YEAR, LS-PARTEDIT-ERRORCOUNTER.
       *Validating PART-NUMBER should NOT be blank
            IF PART-NUMBER = SPACES
                 THEN  ADD +1 TO LS-PARTEDIT-ERRORCOUNTER
@@ -39,22 +64,21 @@
            END-IF.
       *Validating VEHICLE MAKE to be one of the 88 level fields
            EVALUATE TRUE
-              WHEN VEHICLE-MAKE IN PARTS = 'CHR' DISPLAY 'V-MAKE CHR'
-              WHEN VEHICLE-MAKE IN PARTS = 'FOR' DISPLAY 'V-MAKE FOR'
-              WHEN VEHICLE-MAKE IN PARTS = 'GM'  DISPLAY 'V-MAKE GM'
-              WHEN VEHICLE-MAKE IN PARTS = 'VW' DISPLAY 'V-MAKE VW'
-              WHEN VEHICLE-MAKE IN PARTS = 'TOY' DISPLAY 'V-MAKE TOY'
-              WHEN VEHICLE-MAKE IN PARTS = 'JAG' DISPLAY 'V-MAKE JAG'
-              WHEN VEHICLE-MAKE IN PARTS = 'PEU' DISPLAY 'V-MAKE PEU'
-              WHEN VEHICLE-MAKE IN PARTS = 'BMW' DISPLAY 'V-MAKE BMW'
+              WHEN VEHICLE-MAKE = 'CHR' DISPLAY 'V-MAKE CHR'
+              WHEN VEHICLE-MAKE = 'FOR' DISPLAY 'V-MAKE FOR'
+              WHEN VEHICLE-MAKE = 'GM'  DISPLAY 'V-MAKE GM'
+              WHEN VEHICLE-MAKE = 'VW' DISPLAY 'V-MAKE VW'
+              WHEN VEHICLE-MAKE = 'TOY' DISPLAY 'V-MAKE TOY'
+              WHEN VEHICLE-MAKE = 'JAG' DISPLAY 'V-MAKE JAG'
+              WHEN VEHICLE-MAKE = 'PEU' DISPLAY 'V-MAKE PEU'
+              WHEN VEHICLE-MAKE = 'BMW' DISPLAY 'V-MAKE BMW'
               WHEN OTHER ADD +1 TO LS-PARTEDIT-ERRORCOUNTER
            END-EVALUATE.
 
 
-
       *Validating VEHICLE YEAR to be between 1990 and 2019
            COMPUTE WS-VEHICLE-YEAR-INT = FUNCTION NUMVAL
-           (VEHICLE-YEAR IN PARTS).
+           (VEHICLE-YEAR).
            IF WS-VEHICLE-YEAR-INT < 1990 OR
               WS-VEHICLE-YEAR-INT > 2019
               THEN ADD +1 TO LS-PARTEDIT-ERRORCOUNTER
